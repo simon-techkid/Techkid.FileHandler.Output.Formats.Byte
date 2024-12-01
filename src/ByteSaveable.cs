57fd4ad96a8786652a384a8fbba0b6a8cbfa9304
simon-techkid <simon.techkid@outlook.com>
2024-11-30 17:03:44 -0800

    v1.0.0

---
+++ src/ByteSaveable.cs
@@ -0,0 +1,21 @@
+﻿// Techkid.FileHandler by Simon Field
+
+using System;
+
+namespace Techkid.FileHandler.Output.Formats.Byte;
+
+/// <summary>
+/// Provides instructions for serializing an array of bytes.
+/// </summary>
+public abstract class ByteSaveable<TRecord>(Func<TRecord> doc, string? trackName) : SaveableBase<TRecord, byte[], byte[]>(doc, trackName)
+{
+    protected override byte[] ConvertToBytes()
+    {
+        return Document;
+    }
+
+    protected override byte[] ClearDocument()
+    {
+        return Array.Empty<byte>();
+    }
+}
